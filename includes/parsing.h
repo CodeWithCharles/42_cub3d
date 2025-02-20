0fea24ebdf8e094381424bac610bc14c16c1619f
Milan Kaliszczak <mkaliszc@c2r1p3.42lehavre.fr>
2025-02-20 21:51:22 +0100

    + | Assets handling and filename format

---
+++ includes/parsing.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkaliszc <mkaliszc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 21:04:13 by mkaliszc          #+#    #+#             */
+/*   Updated: 2025/02/20 21:25:14 by mkaliszc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PARSING_H
+# define PARSING_H
+
+typedef struct s_bool_format
+{
+	bool	n_texture;
+	bool	s_texture;
+	bool	w_texture;
+	bool	e_texture;
+	bool	f_colors;
+	bool	c_colors;
+	bool	d_texture;
+}	t_bool_format;
+
+#endif
