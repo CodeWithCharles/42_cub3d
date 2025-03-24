6de43efe066e375baa33b0dd07be54eef194105a
Charles POULAIN <charles.probe@gmail.com>
2025-03-24 13:53:38 +0100

    ~ | Small fixes

--- src/parsing/utils.c
+++ src/parsing/utils.c
@@ -6,11 +6,31 @@
 /*   By: cpoulain <cpoulain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 19:26:31 by mkaliszc          #+#    #+#             */
-/*   Updated: 2025/03/21 14:28:30 by cpoulain         ###   ########.fr       */
+/*   Updated: 2025/03/24 13:49:10 by cpoulain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#include "../../includes/cub3d.h"
+
+static int	valid_char(char c)
+{
+	return (c == _FLOOR || c == _WALL || c == _VOID
+		|| ft_strchr(_SPAWNS, c) != 0 || ft_strchr(_DOORS, c) != 0);
+}
+
+static int	validate_line(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line && line[i])
+	{
+		if (valid_char(line[i]) == 0)
+			return (RET_ERR);
+		i++;
+	}
+	return (RET_OK);
+}
 
 char	*skip_space(char *line)
 {
@@ -32,13 +52,11 @@ char	**dup_map(char **map)
 	while (map[count])
 		count++;
 	new_map = malloc((count + 1) * sizeof(char *));
-	if (!new_map)
-		return (NULL);
 	while (i < count)
 	{
-		if (ft_strcmp(map[i], "_") == 0)
+		if (ft_strcmp(map[i], "_") == 0 || validate_line(map[i]) == RET_ERR)
 			invalid = true;
-		new_map[i] = strdup(map[i]);
+		new_map[i] = ft_strdup(map[i]);
 		i++;
 	}
 	new_map[count] = NULL;
